17cf8112c1be232c0ce35b84f36ab9ce9aaef409
NACH0CHEE5E <39394001+NACH0CHEE5E@users.noreply.github.com>
2018-07-22 12:55:29 +1000

    1.4.0

--- !sync.bat
+++ !sync.bat
@@ -1,4 +1,7 @@
-start SyncClassLibary.ffs_gui
-pause
-start SyncModFiles.ffs_gui
+@echo off
+start SyncClassLibary.ffs_batch
+echo syncing class libary
+start SyncModFiles.ffs_batch
+echo syncing mod files with repository
+echo done...
 pause
